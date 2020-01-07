c4722b90c8609bb4d3bd1b04b7f2b51faae59193
sluzzz <franpanpan@gmail.com>
2020-01-07 15:33:30 +0100

    add countries app

---
+++ part2/countries/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Country from './components/Country';
+
+function App() {
+  const [filter, setFilter] = useState('');
+  const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://restcountries.eu/rest/v2/all').then(response => {
+      setCountries(response.data);
+    });
+  }, []);
+
+  const handleChange = e => {
+    setFilter(e.target.value.toLowerCase());
+  };
+
+  let limit = '';
+  let results = filter
+    ? countries.filter(
+        country => country.name.toLowerCase().search(filter) !== -1
+      )
+    : countries;
+
+  if (filter) {
+    results = countries.filter(
+      country => country.name.toLowerCase().search(filter) !== -1
+    );
+    if (results.length > 10) limit = 'Too many matches, specify another filter';
+  }
+
+  return (
+    <div>
+      find countries
+      <input onChange={handleChange} value={filter} />
+      {limit ? (
+        <div>{limit}</div>
+      ) : (
+        results.map(country => <Country key={country.name} country={country} />)
+      )}
+    </div>
+  );
+}
+
+// todo hide starting countries
+
+export default App;
