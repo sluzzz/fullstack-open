453f6b0c45cdb7a27cb50124c6da6d0a9c8a385d
unknown <franpanpan@gmail.com>
2020-01-12 22:42:33 +0100

    update list of numbers to be stateful

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Person from './components/Person';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
+import Notification from './components/Notification';
 import personService from './services/persons';
 
 const App = () => {
@@ -9,12 +10,22 @@ const App = () => {
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+  const [personsFiltered, setPersonsFiltered] = useState([]);
+  const [clicked, setClicked] = useState(false);
+  // const [notification, setNotification] = useState('');
 
   useEffect(() => {
     personService.getAll().then(response => {
       setPersons(response.data);
     });
-  }, []);
+  }, [clicked]);
+
+  useEffect(() => {
+    setPersonsFiltered(
+      persons.filter(person => person.name.toLowerCase().search(filter) !== -1)
+    );
+  }, [filter, persons, clicked]);
+  // todo rerender when clicking on a button
 
   const addPerson = e => {
     e.preventDefault();
@@ -30,12 +41,13 @@ const App = () => {
       }
     }
     personService.post(personObject);
+    //todo put instead of post so json server updates automatically
 
     setNewName('');
     setNewNumber('');
   };
 
-  // todo
+  // todo number change
   // const addPerson = e => {
   //   e.preventDefault();
   //   const personObject = {
@@ -79,19 +91,19 @@ const App = () => {
     setFilter(e.target.value.toLowerCase());
   };
 
-  const results = filter
-    ? persons.filter(person => person.name.toLowerCase().search(filter) !== -1)
-    : persons;
-
   const handleDeleteClick = e => {
-    if (window.confirm(`Delete ${e.target.name}?`)) {
-      personService.del(e.target.id);
-    }
+    // if (window.confirm(`Delete ${e.target.name}?`)) {
+    //   personService.del(e.target.id);
+    // }
+    personService.del(e.target.id);
+
+    setClicked(!clicked);
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={'poruka'} />
       <Filter onChange={handleFilterChange} />
       <h2>add a new</h2>
       <PersonForm
@@ -102,13 +114,25 @@ const App = () => {
         numberVal={newNumber}
       />
       <h2>Numbers</h2>
-      {results.map(person => (
-        <Person key={person.name} person={person} onClick={handleDeleteClick} />
-      ))}
+      {filter
+        ? personsFiltered.map(person => (
+            <Person
+              key={person.name}
+              person={person}
+              onClick={handleDeleteClick}
+            />
+          ))
+        : persons.map(person => (
+            <Person
+              key={person.name}
+              person={person}
+              onClick={handleDeleteClick}
+            />
+          ))}
     </div>
   );
 };
 
-// todo usestate na results, finish 2.18
+// todo finish 2.18
 
 export default App;
